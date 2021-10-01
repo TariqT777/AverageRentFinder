026ae3554069b97f7769c86391e7d5d3420394ef
TariqT777 <TariqThomas777@gmail.com>
2021-09-30 21:08:07 -0500

    May have to webscrape insted of directly pulling data from an API

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(api_url)
-print(response.json())
+url = "https://realty-mole-property-api.p.rapidapi.com/saleListings"
+
+querystring = {"city":"Austin","state":"TX"}
+
+headers = {
+    'x-rapidapi-host': "realty-mole-property-api.p.rapidapi.com",
+    'x-rapidapi-key': "34625615e4msh88c6bc89082c953p12795cjsnc6833b4a870c"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
