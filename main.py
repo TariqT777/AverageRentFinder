99d241bbfabf143874197fcf14d5e01e29aecde2
TariqT777 <TariqThomas777@gmail.com>
2021-10-01 20:19:13 -0500

    imported BeautifulSoup and urllib libraries for webscraping.

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-import requests
-'''
-url = "https://realty-mole-property-api.p.rapidapi.com/saleListings"
-
-querystring = {"city":"Austin","state":"TX"}
-
-headers = {
-    'x-rapidapi-host': "realty-mole-property-api.p.rapidapi.com",
-    'x-rapidapi-key': "34625615e4msh88c6bc89082c953p12795cjsnc6833b4a870c"
-    }
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
-
-'''
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
