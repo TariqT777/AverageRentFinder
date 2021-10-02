0469c9bba40d2b3955994ba077f1434df0f1478e
TariqT777 <TariqThomas777@gmail.com>
2021-10-01 20:30:43 -0500

    Imported a webpage into file and parsed it.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+
+my_url = "https://www.apartments.com/queens-ny/"
+
+uClient = uReq(my_url)
+readPage = uClient.read()
+#print(readPage)
+uClient.close()
+
+
+#html parsing
+page_soup = soup(readPage, "html.parser")
