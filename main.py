a0af42e8d7ed2cb527a895e81a06b81daa3293bc
TariqT777 <TariqThomas777@gmail.com>
2021-10-02 15:24:16 -0500

    Formatted code.

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ from bs4 import BeautifulSoup as soup
 from socket import timeout
 from urllib.error import HTTPError, URLError
 
+def grabURL():
 
-my_url = "https://www.apartments.com/"
+    my_url = "https://www.apartments.com/"
 
-#uClient = uReq(my_url)
-try:
-    response = uReq(my_url, timeout=10).read().decode('utf-8')
-except HTTPError as error:
-    raise Exception('Data not retrieved because URL: ', error, my_url)
-except URLError as error:
-    raise Exception('URL Error')
-except timeout:
-    raise Exception('socket timed out - URL:', my_url)
-else:
-    print('Access successful.')
-readPage = uReq.read()
-#print(readPage)
-uReq.close()
+    #uClient = uReq(my_url)
+    try:
+        response = uReq(my_url, timeout=10).read().decode('utf-8')
+    except HTTPError as error:
+        raise Exception('Data not retrieved because URL: ', error, my_url)
+    except URLError as error:
+        raise Exception('URL Error')
+    except timeout:
+        raise Exception('socket timed out - URL:', my_url)
+    else:
+        print('Access successful.')
+    readPage = uReq.read()
+    #print(readPage)
+    uReq.close()
 
 
-#html parsing
-page_soup = soup(readPage, "html.parser")
+    #html parsing
+    page_soup = soup(readPage, "html.parser")
 
-print(page_soup.h1)
+    print(page_soup.h1)
