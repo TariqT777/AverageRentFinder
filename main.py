19fcb531bd31ee630ad42f818472c7175bd2389f
TariqT777 <TariqThomas777@gmail.com>
2021-10-02 15:20:57 -0500

    Added try-excepts for a timeout,url, and http error.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+from socket import timeout
+from urllib.error import HTTPError, URLError
 
-my_url = "https://www.apartments.com/queens-ny/"
 
-uClient = uReq(my_url)
-readPage = uClient.read()
+my_url = "https://www.apartments.com/"
+
+#uClient = uReq(my_url)
+try:
+    response = uReq(my_url, timeout=10).read().decode('utf-8')
+except HTTPError as error:
+    raise Exception('Data not retrieved because URL: ', error, my_url)
+except URLError as error:
+    raise Exception('URL Error')
+except timeout:
+    raise Exception('socket timed out - URL:', my_url)
+else:
+    print('Access successful.')
+readPage = uReq.read()
 #print(readPage)
-uClient.close()
+uReq.close()
 
 
 #html parsing
 page_soup = soup(readPage, "html.parser")
+
+print(page_soup.h1)
